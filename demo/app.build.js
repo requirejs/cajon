1cb2d7a0c2b2b153f15e39a11c036e093f898860
jrburke <jrburke@gmail.com>
2012-06-13 12:00:43 -0600

    Mention useSourceUrl build option

--- demo/app.build.js
+++ demo/app.build.js
@@ -14,6 +14,20 @@
     //minified content.
     optimize: 'none',
 
+    //Uncomment this line if you want the
+    //built file to use eval with sourceURL
+    //for each module built into the built file.
+    //This can help make debugging of the built file
+    //a little more pleasant in the debugger.
+    //Note however that it only seems to work well
+    //in Chrome at the moment. Firefox may be confused
+    //by the double eval going on.
+    //Also, only use it when not doing minification,
+    //just for debugging only. It also may cause
+    //problems if you are using IE conditional comments
+    //in JavaScript
+    //useSourceUrl: true,
+
     //Instruct the r.js optimizer to
     //convert commonjs-looking code
     //to AMD style, which is needed for
